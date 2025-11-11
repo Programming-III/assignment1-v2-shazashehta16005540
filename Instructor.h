0da0007fb0984db54947a9d614f623990a9e68e4
shazashehta16005540 <shazamohamed8923@gmail.com>
2025-11-11 09:25:35 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,16 +4,33 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
+class instructor:public person{
+private:
+string department;
+int exp;
+public:
+instructor():person(){
+  department ="";
+  exp=0;
+}
+instructor(department ,exp):person(name,id){
+  this->department=department;
+  this->exp=exp;
+}
+~instructor():~person(){
+  
+}
+void display():display(){
+cout<<department<<endl;
+cout<<exp<<endl;
+}
+
+};
 
 
 
 
 
 
-
-
-
-
-
-
+};
 #endif
