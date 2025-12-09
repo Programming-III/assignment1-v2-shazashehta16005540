08cccd1931d030c67e92740a0f8e4204f1cf4f3c
shazashehta16005540 <shazamohamed8923@gmail.com>
2025-12-09 09:39:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,9 +19,33 @@ using namespace std;
 
 // ==================== Student Class Implementation ====================
 
+friend student operator<<(ostream & out,student&s) {
+  out<<s.yearlevel<<","<<s.major<<endl;
+  return out;
+}
+friend instructor operator<<(ostream & out,instructor&i) { 
+  out<<i.department<<","<<i.exp<<endl;
+  return out;
+}
+friend course operator<<(ostream & out,course&c) { 
+  out<<c.coursename<<","<<c.coursecode<<endl;
+  return out;
+}
+student operator[](int i){
+  return student[i];
+}
 
-
-
+bool operator==(course&c){
+  if(this->coursecode=c.coursecode&&
+  this->coursename=c.coursename)
+  return true;
+  else 
+  return false;
+}
+course operator +=(student&s){
+  student.student(s);
+  return*this;
+}
 
 
 // ==================== Instructor Class Implementation ====================
